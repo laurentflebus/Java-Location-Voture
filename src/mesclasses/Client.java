0ed14ccfccc80ae750302860951bc8a21b640376
Laurent <laurentflebus@gmail.com>
2018-10-05 14:59:22 +0200

    classe Voiture version 2

--- src/mesclasses/Client.java
+++ src/mesclasses/Client.java
@@ -28,18 +28,87 @@ public class Client {
        //DELETE FROM Client WHERE date_inscription = this.date_inscription;
        
     }
-    public void modifier(String nom, String prenom, String rue, String n_rue, int code_postal, String ville, String telephone, String email, String sexe, String date_inscription) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.rue = rue;
-        this.n_rue = n_rue;
+
+    public int getCode_postal() {
+        return code_postal;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDate_inscription() {
+        return date_inscription;
+    }
+
+    public String getN_rue() {
+        return n_rue;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setCode_postal(int code_postal) {
         this.code_postal = code_postal;
-        this.ville = ville;
-        this.telephone = telephone;
-        this.email = email;
-        this.sexe = sexe;
+    }
+
+    public void setDate_inscription(String date_inscription) {
         this.date_inscription = date_inscription;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setN_rue(String n_rue) {
+        this.n_rue = n_rue;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+    
     @Override
     public String toString() {
         return "Nom : " + nom + "\n" + 
